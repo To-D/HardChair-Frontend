19da9f1ddafd21290ecb0453bf4c59bc546cb01b
Shen Yunfei <1563407190@qq.com>
2020-03-20 00:17:45 +0800

    update ui for login (trial)

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="base_login">
-    <el-form :model="loginForm"
+    <!-- <el-form :model="loginForm"
              :rules="rules"
              class="login_container"
              label-position="left"
@@ -28,60 +28,116 @@
                      style="width: 40%;background: #afb4db;border: none">register</el-button>
         </router-link>
       </el-form-item>
-    </el-form>
+    </el-form>-->
+    <section class="min-vh-100 py-5">
+      <div class="container">
+        <div class="row justify-content-center mb-md-6">
+          <div class="col-auto">
+            <a>
+              <img src="../assets/img/logo.svg" alt="Leap" />
+            </a>
+          </div>
+        </div>
+        <div class="row justify-content-center pt-6">
+          <div class="col-xl-4 col-lg-5 col-md-6">
+            <div class="text-center mb-4">
+              <h1 class="mb-1">Welcome back</h1>
+              <span>Enter your account details below</span>
+            </div>
+            <form
+              :model="loginForm"
+              :rules="rules"
+              class="login_container"
+              label-position="left"
+              label-width="0px"
+              v-loading="loading"
+            >
+              <div class="form-group" prop="username">
+                <input
+                  type="text"
+                  v-model="loginForm.username"
+                  auto-complete="off"
+                  placeholder="username"
+                  class="form-control"
+                />
+              </div>
+              <div class="form-group" prop="password">
+                <input
+                  type="password"
+                  v-model="loginForm.password"
+                  auto-complete="off"
+                  placeholder="password"
+                  class="form-control"
+                />
+              </div>
+              <div class="form-group">
+                <button v-on:click="login" class="btn-block btn btn-primary" type="submit">Sign in</button>
+              </div>
+              <hr />
+              <div class="text-center text-small text-muted">
+                <span>
+                  Don't have an account yet?
+                  <router-link to="register">Create one</router-link>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
-
 </template>
 
 <script>
 export default {
-  name: 'Login',
-  data () {
+  name: "Login",
+  data() {
     return {
       loginForm: {
-        username: '',
-        password: ''
+        username: "",
+        password: ""
       },
       rules: {
-        username: [{required: true, message: '', trigger: 'blur'}],
-        password: [{required: true, message: '', trigger: 'blur'}]
+        username: [{ required: true, message: "", trigger: "blur" }],
+        password: [{ required: true, message: "", trigger: "blur" }]
       },
       loading: false
-    }
+    };
   },
   methods: {
-    login () {
-      this.$axios.post('/login', {
-        username: this.loginForm.username,
-        password: this.loginForm.password
-      })
+    login() {
+      this.$axios
+        .post("/login", {
+          username: this.loginForm.username,
+          password: this.loginForm.password
+        })
         .then(resp => {
           if (resp.status === 200 && resp.data.hasOwnProperty("token")) {
-            this.$store.commit('login', resp.data)
-            this.$router.replace({path: '/'})
-          } else{
-            alert('login error')
+            this.$store.commit("login", resp.data);
+            this.$router.replace({ path: "/" });
+          } else {
+            alert("login error");
           }
         })
         .catch(error => {
-          console.log(error)
-          alert('login error')
-        })
+          console.log(error);
+          alert("login error");
+        });
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  #base_login{
+/* #base_login{
     background: url("../assets/background/checkerboard-cross.png") repeat;
     background-position: center;
     height: 100%;
     width: 100%;
     background-size: cover;
     position: fixed;
-  }
-  body{
+  } */
+/* body{
     margin: 0px;
     padding: 0px;
   }
@@ -99,5 +155,5 @@ export default {
     margin: 0px auto 40px auto;
     text-align: center;
     color: #494e8f;
-  }
+  } */
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import {router} from './router'
 import store from './store'
 import ElementUI from 'element-ui'
+import './assets/css/theme.css'
 
 Vue.use(ElementUI)
 
