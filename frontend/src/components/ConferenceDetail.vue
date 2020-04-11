b6ee79b39fa86f705959136a6e46b167d92eba2b
Shen Yunfei <1563407190@qq.com>
2020-04-12 00:57:19 +0800

    update paper submission form (to be debugged)

--- frontend/src/components/ConferenceDetail.vue
+++ frontend/src/components/ConferenceDetail.vue
@@ -6,8 +6,8 @@
       <div class="container py-0 layer-2">
         <div class="row my-4 my-md-6 text-light">
           <div class="col-lg-9 col-xl-6">
-            <h1 class="display-4">{{conference.nameAbbreviation}}</h1>
-            <p class="lead mb-0">{{conference.fullName}}</p>
+            <h1 class="display-4">{{conference.title}}</h1>
+            <p class="lead mb-0">{{conference.summary}}</p>
           </div>
         </div>
       </div>
@@ -18,128 +18,68 @@
         <div class="row">
           <div class="col-xl-8 col-lg-8">
             <div>
-              <h2><i class="el-icon-info"></i> Conference Info</h2>
-              <div>
+              <h2>
+                <i class="el-icon-info"></i> Conference Info
+              </h2>
+              <div class="infoitem">
                 <span class="itemlabel">
                   <i class="el-icon-user-solid"></i> Application by:
                 </span>
                 {{conference.owner}}
               </div>
-              <div>
+              <div class="infoitem">
                 <span class="itemlabel">
                   <i class="el-icon-chat-dot-round"></i> Short name:
                 </span>
-                {{conference.nameAbbreviation}}
+                {{conference.title}}
               </div>
-              <div>
+              <div class="infoitem">
                 <span class="itemlabel">
                   <i class="el-icon-chat-line-round"></i> Full name:
                 </span>
-                {{conference.fullName}}
+                {{conference.summary}}
               </div>
-              <div>
+              <div class="infoitem">
                 <span class="itemlabel">
                   <i class="el-icon-location"></i> Location:
                 </span>
                 {{conference.location}}
               </div>
-              <div>
+              <div class="infoitem">
                 <span class="itemlabel">
                   <i class="el-icon-video-play"></i> Starts at:
                 </span>
                 {{conference.startTime.substring(0,10)}}
               </div>
-              <div>
+              <div class="infoitem">
                 <span class="itemlabel">
                   <i class="el-icon-video-pause"></i> Ends at:
                 </span>
                 {{conference.endTime.substring(0,10)}}
               </div>
-              <div>
+              <div class="infoitem">
                 <span class="itemlabel">
                   <i class="el-icon-date"></i> Submission deadline:
                 </span>
                 {{conference.deadline.substring(0,10)}}
               </div>
-              <div>
+              <div class="infoitem">
                 <span class="itemlabel">
                   <i class="el-icon-medal-1"></i> Result announcement at:
                 </span>
                 {{conference.resultAnnounceDate.substring(0,10)}}
               </div>
+              <div class="infoitem">
+                <span class="itemlabel">
+                  <i class="el-icon-s-flag"></i> Status:
+                </span>
+                {{parseStatus(conference.status)}}
+              </div>
             </div>
-            <!-- <el-collapse v-model="activeNames" @change="handleChange">
-              <el-collapse-item title="Short name" name="1">
-                <div>{{conference.nameAbbreviation}}</div>
-              </el-collapse-item>
-              <el-collapse-item title="Full name" name="2">
-                <div>{{conference.fullName}}</div>
-              </el-collapse-item>
-              <el-collapse-item title="Location" name="3">
-                <div>{{conference.location}}</div>
-              </el-collapse-item>
-              <el-collapse-item title="Start and end dates" name="4">
-                <div>Starts at {{ conference.startTime.substring(0, 10) }}</div>
-                <div>Ends at {{ conference.endTime.substring(0, 10) }}</div>
-              </el-collapse-item>
-              <el-collapse-item title="Submission deadline" name="5">
-                <div>{{ conference.deadline.substring(0, 10) }}</div>
-              </el-collapse-item>
-              <el-collapse-item title="Result announcement date" name="6">
-                <div>{{ conference.resultAnnounceDate.substring(0, 10) }}</div>
-              </el-collapse-item>
-
-              <el-collapse-item title="Status" name="7">
-                <div>{{conference.status}}</div>
-              </el-collapse-item>
-
-              <el-collapse-item title="Owner" name="8">
-                <div>{{conference.owner}}</div>
-              </el-collapse-item>
-              <el-collapse-item title="Submit your paper" name="8"> -->
-                <!-- <el-form
-                  @submit.native.prevent
-                  status-icon
-                  :model="loginForm"
-                  :rules="rules"
-                  class="login_container"
-                  label-position="left"
-                  label-width="0px"
-                  v-loading="loading"
-                >-->
-                <!-- <el-form-item prop="username" size="medium">
-                    <el-input
-                      size="medium"
-                      type="text"
-                      v-model="loginForm.username"
-                      auto-complete="off"
-                      placeholder="Username"
-                    ></el-input>
-                  </el-form-item>
-
-                  <el-form-item prop="password" size="medium">
-                    <el-input
-                      size="medium"
-                      type="password"
-                      v-model="loginForm.password"
-                      auto-complete="off"
-                      placeholder="Password"
-                    ></el-input>
-                  </el-form-item>
-
-                  <el-form-item size="medium">
-                    <el-button
-                      native-type="submit"
-                      :disabled="isDisabled"
-                      size="medium"
-                      type="primary"
-                      style="width:100% "
-                      v-on:click="login"
-                    >Submit</el-button>
-                </el-form-item>-->
-                <!-- </el-form> -->
-              <!-- </el-collapse-item>
-            </el-collapse> -->
+            <br />
+            <div>
+              <el-button>Start accepting papers</el-button>
+            </div>
           </div>
         </div>
       </div>
@@ -148,7 +88,58 @@
     <section>
       <div class="container">
         <div class="row">
-          <div class="col-xl-6 col-lg-8"></div>
+          <div class="col-xl-8 col-lg-8">
+            <div>
+              <h2>
+                <i class="el-icon-document"></i> Paper submission
+              </h2>
+              <el-form
+                @submit.native.prevent
+                status-icon
+                :model="paperForm"
+                :rules="rules"
+                label-position="top"
+                v-loading="loading"
+                :ref="paperForm"
+              >
+                <!-- short name -->
+                <el-form-item prop="title" label="Title">
+                  <el-input
+                    type="text"
+                    v-model="paperForm.title"
+                    auto-complete="off"
+                    id="title"
+                    placeholder="Title of your paper"
+                  ></el-input>
+                </el-form-item>
+
+                <!--full name -->
+                <el-form-item prop="summary" label="Summary">
+                  <el-input
+                    type="textarea"
+                    autosize
+                    v-model="paperForm.summary"
+                    auto-complete="off"
+                    id="summary"
+                    placeholder="Summary of your paper"
+                  ></el-input>
+                </el-form-item>
+
+                <br />
+
+                <!-- submit button -->
+                <el-form-item style="width: 100%">
+                  <el-button
+                    native-type="submit"
+                    :disabled="isDisabled"
+                    type="primary"
+                    style="width: 100%"
+                    v-on:click="apply(paperForm)"
+                  >Submit paper</el-button>
+                </el-form-item>
+              </el-form>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -166,22 +157,9 @@ export default {
   components: { navbar, footerbar },
   data() {
     return {
-      // activeNames: ["1", "2", "3", "4", "5", "6", "7", "8"],
       authority: [],
       conference: null
     };
-  },
-  methods: {
-    handleChange(val) {
-      console.log(val);
-    }
-    // parseDate(timestamp) {
-    //   if (timestamp = "") {
-    //     return "N/A";
-    //   } else {
-    //     return timestamp.substring(0, 10);
-    //   }
-    // },
   },
   created() {
     //获取会议信息
@@ -199,6 +177,109 @@ export default {
       .catch(error => {
         console.log(error);
       });
+  },
+  data: function() {
+    //Validators
+
+    //Judgement whether the submit button can work when the from changes
+    const isFormReady = (rule, value, callback) => {
+      callback();
+      this.changeDisabled();
+    };
+
+    return {
+      isDisabled: true, //Control the function of the submit button
+
+      paperForm: {
+        title: "",
+        summary: ""
+      },
+      rules: {
+        title: [
+          {
+            required: true,
+            message: "Title of paper is required",
+            trigger: "blur"
+          },
+          { validator: isFormReady, trigger: "change" }
+        ],
+        summary: [
+          {
+            required: true,
+            message: "Summary of paper is required",
+            trigger: "blur"
+          },
+          { validator: isFormReady, trigger: "change" }
+        ]
+      },
+      loading: false
+    };
+  },
+  methods: {
+    apply(formName) {
+      //In case of some bug, still validate before submit
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.loading = true;
+          this.$axios
+            .post("/SubmitPaper", {
+              title: this.paperForm.title,
+              summary: this.paperForm.summary,
+            })
+            .then(resp => {
+              // 根据后端的返回数据修改
+              if (resp.status === 200 && resp.data.hasOwnProperty("id")) {
+                this.loading = false;
+                this.$message({
+                  type: "success",
+                  center: true,
+                  dangerouslyUseHTMLString: true,
+                  message:
+                    "<strong style='color:teal'>Submission successful!</strong>"
+                });
+                this.$router.replace("/");
+              } else {
+                this.$message.error("Submission failed. Please sign in first");
+              }
+            })
+            .catch(error => {
+              this.loading = false;
+              console.log(error);
+              this.$message.error("Submission failed");
+            });
+        } else {
+          this.$message.error("Wrong submit! Please check the form.");
+        }
+      });
+    },
+    changeDisabled() {
+      this.isDisabled =
+        this.paperForm.title === "" ||
+        this.paperForm.summary === "";
+    },
+    parseStatus(status) {
+      switch (status) {
+        case "CHECKED":
+          return "Approved by admin";
+          break;
+        case "SUBMIT_ALLOWED":
+          return "Accepting papers";
+          break;
+        default:
+          return "Currently unknown";
+          break;
+      }
+    },
+    handleChange(val) {
+      console.log(val);
+    }
+    // parseDate(timestamp) {
+    //   if (timestamp = "") {
+    //     return "N/A";
+    //   } else {
+    //     return timestamp.substring(0, 10);
+    //   }
+    // },
   }
 };
 </script>
@@ -207,11 +288,13 @@ export default {
 section {
   padding: 2em;
 }
-.itemlabel, h2 {
-  /* background-color: #3755be; */
+.itemlabel,
+h2 {
   color: #3755be;
   font-weight: bold;
-  /* padding: 0.1em 0.3em;
-  margin-right: 0.3em; */
+}
+.infoitem {
+  margin-top: 1em;
+  margin-bottom: 1em;
 }
 </style>
