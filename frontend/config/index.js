946c621da10577a56add1de2fefed194878e08db
Shen Yunfei <1563407190@qq.com>
2020-04-13 04:05:44 +0800

    test run config host address

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'http://114.115.164.240', // can be overwritten by process.env.HOST
+    host: '114.115.164.240', // can be overwritten by process.env.HOST
     port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
