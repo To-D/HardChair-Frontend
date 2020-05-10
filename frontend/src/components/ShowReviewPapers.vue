75253ec8c9bd775e8c287681869273320c51725e
Shen Yunfei <1563407190@qq.com>
2020-05-10 20:28:25 +0800

    [Fix] review papers list style

--- frontend/src/components/ShowReviewPapers.vue
+++ frontend/src/components/ShowReviewPapers.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+  <div>
     <div v-if="papers.length == 0">
       <el-card shadow="hover">No paper now!</el-card>
     </div>
@@ -30,9 +30,11 @@
         {{paper.createdTime.substring(0,10)}}
       </p>
       <!-- paper operation -->
-      <preview :id="paper.id">Preview</preview>
-      <download :id="paper.id" :title="paper.title"></download>
-      <el-button @click="$router.push({path:'/paper/'+paper.id}) ">Edit</el-button>
+      <div class="row">
+        <preview class="onPageBtn" :id="paper.id">Preview</preview>
+        <download class="onPageBtn" :id="paper.id" :title="paper.title"></download>
+        <el-button class="onPageBtn" type="primary" @click="$router.push({path:'/paper/'+paper.id}) ">Review</el-button>
+      </div>
     </el-card>
     <div class="row">
       <div class="col-xl-6 col-lg-12">
@@ -45,7 +47,7 @@
         >></el-pagination>
       </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
@@ -54,25 +56,61 @@ import preview from "./PreviewPaper";
 
 export default {
   name: "ShowPapers",
-  components:{download,preview},
-  props:['conferenceId'],
-  data(){
-      return{
-         pageSize: 2,
-         currentPage: 1,
-         papers:[],
-      }
+  components: { download, preview },
+  props: ["conferenceId"],
+  data() {
+    return {
+      pageSize: 2,
+      currentPage: 1,
+      papers: []
+    };
   },
-  created(){
-      this.$axios.post('/ReviewPapers',{
-          conferenceId:this.conferenceId,
+  created() {
+    this.$axios
+      .post("/ReviewPapers", {
+        conferenceId: this.conferenceId
       })
-      .then(resp=>{
-          this.papers = resp.data;
+      .then(resp => {
+        this.papers = resp.data;
       })
-      .catch(error=>{
-          console.log(error);
-      })
-  },
+      .catch(error => {
+        console.log(error);
+      });
+  }
 };
 </script>
+
+<style scoped>
+section {
+  padding: 0.5em;
+}
+.itemlabel,
+h2 {
+  color: #3755be;
+  font-weight: bold;
+}
+.infoitem {
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+.contentContainer {
+  padding: 2em;
+}
+.onPageBtn {
+  margin-left: 12px;
+}
+el-form-item {
+  margin-bottom: 0;
+}
+.el-tag {
+  margin-right: 5px;
+}
+.input-new-tag {
+  width: 103px;
+  margin-right: 5px;
+  vertical-align: bottom;
+}
+.checkboxes {
+  margin-right: 0;
+}
+</style>
