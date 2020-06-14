5923a4d33d92c3c5f0ac940bada33c93cce35c81
Shen Yunfei <1563407190@qq.com>
2020-06-14 17:16:35 +0800

    [Change] some format for forum

--- frontend/src/components/Forum.vue
+++ frontend/src/components/Forum.vue
@@ -1,111 +1,114 @@
 <template>
-<div>
+  <div>
     <!-- reply area -->
-    <div v-if="canDiscuss" ref="reply_area">    
-        <p>Reply Area</p>
-        <br/>
-        <div v-if="quoteContent">
-            <p>{{quoteContent}}</p>
-            <el-button @click= "quoteContent='';quoteId=-1">Cancel quoting</el-button>
-        </div>
-        <el-input
-            type="textarea"
-            autosize
-            size="medium"
-            v-model="postContent"
-            auto-complete="off"
-            maxlength="500"
-            show-word-limit
-            placeholder="Enter what you want to say"
-        ></el-input>
-        <el-button :disabled="submitDisable" @click="submitPost">Submit</el-button>
-    </div>    
-    <el-card shadow="hover" v-else>It's not the time for discussing !</el-card>
+    <div v-if="canDiscuss" ref="reply_area">
+      <p>Reply Area</p>
+      <br />
+      <div v-if="quoteContent">
+        <p>{{quoteContent}}</p>
+        <el-button @click="quoteContent='';quoteId=-1">Cancel quoting</el-button>
+      </div>
+      <el-input
+        type="textarea"
+        autosize
+        size="medium"
+        v-model="postContent"
+        auto-complete="off"
+        maxlength="500"
+        show-word-limit
+        placeholder="What do you think of the essay?"
+      ></el-input>
+      <el-button :disabled="submitDisable" @click="submitPost">Submit</el-button>
+    </div>
+    <el-card shadow="hover" v-else>Discussion is not available now. Please try at a later date.</el-card>
 
-    <br>
+    <br />
 
     <!-- post area -->
     <div v-if="posts">
-        <el-card
-            shadow="hover"
-            class="box-card"
-            style="margin-top: 1em;"
-            v-for="(post,index) in posts.slice((currentPage- 1)*pageSize,currentPage*pageSize)"
-            :key="index"
-        >
-            <!-- header -->
-            <div slot="header" class="clearfix">
-                <span>{{post.username}}</span>
-                <span style="float: right; padding: 3px 0" v-if= "post.createdTime">{{post.createdTime.substr(0,10)}} {{post.createdTime.substr(11,8)}}</span>
-            </div>
-            <div v-if="post.quoteId != -1">{{getQuoteContent(post.quoteId)}}</div>
-            <div>{{post.postContent}}</div>
-            <el-button v-if="canDiscuss" @click="reply(post)">Reply</el-button>
-        </el-card>
+      <el-card
+        shadow="hover"
+        class="box-card"
+        style="margin-top: 1em;"
+        v-for="(post,index) in posts.slice((currentPage- 1)*pageSize,currentPage*pageSize)"
+        :key="index"
+      >
+        <!-- header -->
+        <div slot="header" class="clearfix">
+          <span>{{post.username}}</span>
+          <el-button
+            v-if="canDiscuss"
+            style="float: right; padding: 3px 0"
+            type="text"
+            @click="reply(post)"
+          >Reply</el-button>
+        </div>
+        <div v-if="post.quoteId != -1">{{getQuoteContent(post.quoteId)}}</div>
+        <div>{{post.postContent}}</div>
+      </el-card>
     </div>
 
-    <br>
+    <br />
 
     <!-- paging -->
     <el-pagination
-        v-if = "posts.length>0"
-        hide-on-
-        single-page
-        layout="prev, pager, next"
-        :page-size="pageSize"
-        :current-page.sync="currentPage"
-        :total="posts.length"
+      v-if="posts.length>0"
+      hide-on-
+      single-page
+      layout="prev, pager, next"
+      :page-size="pageSize"
+      :current-page.sync="currentPage"
+      :total="posts.length"
     ></el-pagination>
-
-</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "forum",
-  props:["canDiscuss","posts","paperId"],
-  inject:["reload"],
+  props: ["canDiscuss", "posts", "paperId"],
+  inject: ["reload"],
 
   data() {
     return {
-        postContent:"",
-        quoteId:-1,
-        quoteContent:"",
+      postContent: "",
+      quoteId: -1,
+      quoteContent: "",
 
-        pageSize: 6,
-        currentPage: 1,
-    }
-  },  
-  computed:{
-    submitDisable() {
-        return this.postContent == "";
-    },
+      pageSize: 6,
+      currentPage: 1
+    };
   },
-  methods:{
-      reply(post) {
-        this.quoteId = post.id;
-        this.quoteContent = post.username + " : " + post.postContent;
-        this.$refs.reply_area.scrollIntoView({
-            block: "start",
-            inline: "nearest",
-            behavior: "smooth"
+  computed: {
+    submitDisable() {
+      return this.postContent == "";
+    }
+  },
+  methods: {
+    reply(post) {
+      this.quoteId = post.id;
+      this.quoteContent = post.username + " : " + post.postContent;
+      this.$refs.reply_area.scrollIntoView({
+        block: "start",
+        inline: "nearest",
+        behavior: "smooth"
       });
     },
-    getQuoteContent(quoteId){
-        let len = this.posts.length;
-        for(let i = 0; i< len; i++){
-            if(this.posts[i].id == quoteId){
-                return this.posts[i].postContent;
-            }
+    getQuoteContent(quoteId) {
+      let len = this.posts.length;
+      for (let i = 0; i < len; i++) {
+        if (this.posts[i].id == quoteId) {
+          return this.posts[i].postContent;
         }
+      }
     },
-    notify(content,format){
+    notify(content, format) {
       this.$message({
         dangerouslyUseHTMLString: true,
         type: format,
-        message:'<strong style="color:teal">'+content+'</strong>',
+        message: '<strong style="color:teal">' + content + "</strong>",
         center: true
-      });      
+      });
     },
     submitPost() {
       this.$axios
@@ -118,14 +121,17 @@ export default {
           if (resp.status === 200) {
             switch (resp.data.message) {
               case "success":
-                this.notify("Reply successfully!","success");
+                this.notify("Reply successfully!", "success");
                 this.reload();
                 break;
               case "No Authority":
-                this.notify("You don\'t hava the authority!","error");
+                this.notify("You don't hava the authority!", "error");
                 break;
               case "fail: conference status":
-                this.notify("You cannot submit at this conference stage!","error");
+                this.notify(
+                  "You cannot submit at this conference stage!",
+                  "error"
+                );
                 break;
             }
           }
@@ -135,8 +141,8 @@ export default {
         });
       this.postContent = "";
       this.quoteContent = "";
-      this.quoteId=-1;
-    },
-  },
+      this.quoteId = -1;
+    }
+  }
 };
 </script>
